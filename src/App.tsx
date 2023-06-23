affb815799884103f2bdfcc62ffbcc7729a884c4
ParkSeoJune <tjwns4122@gmail.com>
2023-06-23 14:09:30 +0900

    add: toast setting

--- src/App.tsx
+++ src/App.tsx
@@ -12,7 +12,7 @@ import QuestionCommentPage from "./pages/QuestionCommentPage";
 import QuestionListPage from "./pages/QuestionListPage";
 import DiaryListPage from "./pages/DiaryListPage";
 import HeartLevelPage from "./pages/HeartLevelPage";
-import { useRecoilState } from "recoil";
+import { RecoilRoot, useRecoilState } from "recoil";
 import { loggedAtom } from "./atoms/AtomContainer";
 import FindPwIdPage from "./pages/FindPwIdPage";
 import FindIdPage from "./pages/FindIdPage";
@@ -32,25 +32,31 @@ function App() {
 
   return (
     <>
-      <ToastContainer />
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/find" element={<FindPwIdPage />} />
-        <Route path="/findId" element={<FindIdPage />} />
-        <Route path="/findPw" element={<FindPwPage />} />
-        <Route path="/changePw" element={<ChangePwPage />} />
-        <Route path="/linkcouple" element={<LinkCouplePage />} />
-        <Route path="/setdate" element={<SetDatePage />} />
-        <Route path="/diary" element={<DiaryDetailPage />} />
-        <Route path="/createDiary" element={<CreateDiaryPage />} />
-        <Route path="/questionComment" element={<QuestionCommentPage />} />
-        <Route path="/questionList" element={<QuestionListPage />} />
-        <Route path="/diaryList" element={<DiaryListPage />} />
-        <Route path="/heartLevel" element={<HeartLevelPage />} />
-        <Route path="/setting" element={<SettingPage />} />
-      </Routes>
+      <RecoilRoot>
+        <ToastContainer
+          position="top-right"
+          closeButton={false}
+          autoClose={600}
+        />
+        <Routes>
+          <Route path="/" element={<MainPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/signup" element={<SignUpPage />} />
+          <Route path="/find" element={<FindPwIdPage />} />
+          <Route path="/findId" element={<FindIdPage />} />
+          <Route path="/findPw" element={<FindPwPage />} />
+          <Route path="/changePw" element={<ChangePwPage />} />
+          <Route path="/linkcouple" element={<LinkCouplePage />} />
+          <Route path="/setdate" element={<SetDatePage />} />
+          <Route path="/diary" element={<DiaryDetailPage />} />
+          <Route path="/createDiary" element={<CreateDiaryPage />} />
+          <Route path="/questionComment" element={<QuestionCommentPage />} />
+          <Route path="/questionList" element={<QuestionListPage />} />
+          <Route path="/diaryList" element={<DiaryListPage />} />
+          <Route path="/heartLevel" element={<HeartLevelPage />} />
+          <Route path="/setting" element={<SettingPage />} />
+        </Routes>
+      </RecoilRoot>
     </>
   );
 }
