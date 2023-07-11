b743c8a21425eafff083acee55395401776557af
minjekang <kangminje777@gmail.com>
2023-07-11 20:24:47 +0900

    fix: 기념일 페이지 ui수정

--- src/components/Anniversary/AnniversaryList/style.ts
+++ src/components/Anniversary/AnniversaryList/style.ts
@@ -1,6 +1,7 @@
 import styled from "styled-components";
 
 export const Layout = styled.div`
+  position: relative;
   margin-top: 35px;
   display: flex;
   flex-direction: column;
@@ -11,7 +12,7 @@ export const Layout = styled.div`
 export const Anniversary = styled.div`
   width: 100%;
   height: 90px;
-  box-shadow: 0px 2px 15px #f5cacb;
+  box-shadow: 0px 1px 6px #f5cacb;
   background: #ffffff;
   border-radius: 18px;
   margin-top: 20px;
@@ -29,14 +30,20 @@ export const Anniversary = styled.div`
 `;
 
 export const Add = styled.div`
+  position: absolute;
+  top: -64px;
+  right: 0;
   width: 20px;
   height: 20px;
-  padding: 10px;
+  padding: 6px;
   border-radius: 10px;
+  box-shadow: 1px 2px 5px #f5cacb;
+  background: #fff;
   cursor: pointer;
-  background: #f5cacb;
   transition: 0.2s;
   &:hover {
-    transform: scale(1.1);
+    background: #f5cacb;
+    box-shadow: none;
+    transform: scale(1.15);
   }
 `;
